303ad88fdb95d9e77bd371a258acf717bfffb715
phisaw <57394869+phisaw@users.noreply.github.com>
2020-07-04 12:21:19 +0200

    attempt docker image creation

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -3,5 +3,6 @@ public class Project {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("test");
+        System.out.println("3rd line");
     }
 }
